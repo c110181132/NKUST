0a69e8c4abe10ef27a68c18f2d1a79d41587e075
c110181132 <93572393+c110181132@users.noreply.github.com>
2021-11-09 16:38:41 +0800

    Add files via upload

---
+++ alexl.java
@@ -0,0 +1,8 @@
+public class alexl {
+    public static void main(String[] args) {
+        float height = 1.72f;
+        int weight= 50;
+        float bmi=(weight)/(height*height);
+        System.out.println(bmi);
+    }
+}
